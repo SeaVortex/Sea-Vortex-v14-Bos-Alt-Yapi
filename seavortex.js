23bb535eb0dc9cb7e1d3cf973ebdebb2bd9da61a
Sea Vortex <70918063+SeaVortex@users.noreply.github.com>
2022-08-30 01:00:05 +0300

    Update seavortex.js

--- seavortex.js
+++ seavortex.js
@@ -1,5 +1,35 @@
-const { Client, Collection } = require("discord.js")
-const client = (global.client = new Client({ intents: [98303] }))
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js")
+const client = global.client = new Client({
+    fetchAllMembers: true,
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.MessageContent,
+    ],
+    partials: [
+        Partials.Message,
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.Reaction,
+        Partials.GuildScheduledEvent,
+        Partials.User,
+        Partials.ThreadMember,
+    ],
+
+})
 client.commands = new Collection();
 client.aliases = new Collection();
 client.cooldown = new Collection()
