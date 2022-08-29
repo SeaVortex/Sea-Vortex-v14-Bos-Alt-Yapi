e737747d40e4533c9b56e75f8169fa0e80e76a42
Sea Vortex <70918063+SeaVortex@users.noreply.github.com>
2022-08-30 00:59:04 +0300

    Update commandHandler.js

--- src/events/commandHandler.js
+++ src/events/commandHandler.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder, Client, Collection } = require("discord.js");
 const client = global.client;
-const vortex = require("../settings/vortex.js");
+const { panels } = require("../settings/vortex.js");
 let sended = false;
 setInterval(() => {
     client.cooldown.forEach((x, index) => {
@@ -9,15 +9,15 @@ setInterval(() => {
 }, 5000);
 module.exports = async (message) => {
 
-    if (message.author.bot || !message.guild) return;
-    const args = message.content.slice(vortex.panels.prefix.length).split(/ +/);
-    const commandName = args[0].toLowerCase();
+    const prefix = panels.prefix.find((x) => message.content.toLowerCase().startsWith(x));
+    if (message.author.bot || !message.guild || !prefix) return;
+    const args = message.content.substring(prefix.length).trim().split(" ");
+    const commandName = args.shift().toLowerCase();
     const timeout = (v) => setTimeout(() => { v.delete(); }, 5000);
     const seavortex = "728360891644641303"
     const embed = new EmbedBuilder().setColor('#DD0909').setTimestamp().setAuthor({ name: message.member.displayName, iconURL: message.author.avatarURL({ dynamic: true }) }).setFooter({ text: client.users.cache.get(seavortex).tag, iconURL: client.users.cache.get(seavortex).avatarURL({ dynamic: true }) })
-    const cmd = client.commands.has(commandName) ? client.commands.get(commandName) : client.commands.get(client.aliases.get(commandName));
-    if (cmd) {
-        if (cmd.conf.owner && !vortex.panels.botOwner.includes(message.author.id)) return;
+    const cmd = client.commands.has(commandName) ? client.commands.get(commandName) : client.commands.get(client.aliases.get(commandName));    if (cmd) {
+        if (cmd.conf.owner && !panels.botOwner.includes(message.author.id)) return;
         const cooldown = cmd.conf.cooldown || 3000;
         const cd = client.cooldown.get(message.author.id);
         if (cd) {
@@ -34,3 +34,5 @@ module.exports = async (message) => {
 module.exports.event = {
     name: "messageCreate"
 }
+
+
